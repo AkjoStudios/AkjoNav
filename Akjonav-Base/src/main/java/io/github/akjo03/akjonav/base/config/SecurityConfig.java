6e98ee2712dab90c8e23b63a85a9b9133e218b0f
Akjo03 <lukas.kueffer@outlook.com>
2022-07-01 15:04:41 +0200

    Improved security manager for rest services

--- Akjonav-Base/src/main/java/io/github/akjo03/akjonav/base/config/SecurityConfig.java
+++ Akjonav-Base/src/main/java/io/github/akjo03/akjonav/base/config/SecurityConfig.java
@@ -12,13 +12,18 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig implements AuthenticationManager {
+public class SecurityConfig extends BasicAuthenticationEntryPoint implements AuthenticationManager {
 	@Bean
 	public SecurityFilterChain filterChain(@NotNull HttpSecurity http) throws Exception {
 		return http
@@ -31,7 +36,9 @@ public class SecurityConfig implements AuthenticationManager {
 						"/api-docs",
 						"/api-docs/**"
 				).permitAll()
+				.antMatchers("/**").authenticated()
 				.and().httpBasic()
+				.authenticationEntryPoint(this)
 				.and().build();
 	}
 
@@ -40,10 +47,24 @@ public class SecurityConfig implements AuthenticationManager {
 		String username = authentication.getPrincipal().toString();
 		String password = authentication.getCredentials().toString();
 
-		// TODO: Implement authentication
+
 
 		List<GrantedAuthority> grantedAuths = new ArrayList<>();
 		grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
 		return new UsernamePasswordAuthenticationToken(username, password, grantedAuths);
+	}
+
+	@Override
+	public void commence(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull AuthenticationException authException) throws IOException {
+		response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		PrintWriter writer = response.getWriter();
+		writer.println("HTTP Status 401 - " + authException.getMessage());
+	}
+
+	@Override
+	public void afterPropertiesSet() {
+		setRealmName("AkjoNav");
+		super.afterPropertiesSet();
 	}
 }
