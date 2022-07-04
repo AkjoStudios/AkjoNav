ec2ea543b6305264b8610004229b173ac405b969
Akjo03 <lukas.kueffer@outlook.com>
2022-07-04 10:23:03 +0200

    Removed additional properties and added new property configuration for Auth0

---
+++ Akjonav-Base/src/main/java/io/github/akjo03/akjonav/base/config/properties/AuthProperties.java
@@ -0,0 +1,15 @@
+package io.github.akjo03.akjonav.base.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "auth0")
+@Getter
+@Setter
+public class AuthProperties {
+	private String domain;
+	private String audience;
+}
